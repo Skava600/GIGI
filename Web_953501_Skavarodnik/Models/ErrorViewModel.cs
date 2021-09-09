987c4156b9e98163069c4d6d227efa64f8c282ee
Skava600 <56450477+Skava600@users.noreply.github.com>
2021-09-09 16:29:44 +0300

    initial commit

---
+++ Web_953501_Skavarodnik/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Web_953501_Skavarodnik.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
