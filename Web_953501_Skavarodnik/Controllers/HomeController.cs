8d07fcb1f3c709a9d0bb71fa0d49436d6092222e
Skava600 <uskava7@gmail.com>
2021-09-23 14:44:55 +0300

    complete: Lab 2

--- Web_953501_Skavarodnik/Controllers/HomeController.cs
+++ Web_953501_Skavarodnik/Controllers/HomeController.cs
@@ -1,37 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Web_953501_Skavarodnik.Models;
 
 namespace Web_953501_Skavarodnik.Controllers
 {
+    public class ListDemo
+    {
+        public int ListItemValue { get; set; }
+        public string ListItemText { get; set; }
+    }
+
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        [ViewData]
+        public string Text { get; set; }
+        private List<ListDemo> _listDemo;
+        public HomeController()
         {
-            _logger = logger;
+            _listDemo = new List<ListDemo>
+            {
+                new ListDemo{ ListItemValue=1, ListItemText="Item 1"},
+                new ListDemo{ ListItemValue=2, ListItemText="Item 2"},
+                new ListDemo{ ListItemValue=3, ListItemText="Item 3"}
+            };
         }
-
-        public IActionResult Index()
+            public IActionResult Index()
         {
+            Text = "Лабораторная работа 2";
+            ViewData["Lst"] = new SelectList(_listDemo, "ListItemValue", "ListItemText");
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
