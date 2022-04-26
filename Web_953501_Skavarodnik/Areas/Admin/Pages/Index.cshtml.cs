26710fa574d1a4b434325d14b22864e0c76efdd4
Skava600 <uskava7@gmail.com>
2022-04-26 13:46:28 +0300

    minor changes.

--- Web_953501_Skavarodnik/Areas/Admin/Pages/Index.cshtml.cs
+++ Web_953501_Skavarodnik/Areas/Admin/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using Web_953501_Skavarodnik.Entities;
 
 namespace Web_953501_Skavarodnik.Areas.Admin.Pages
 {
+    [Authorize(Roles ="admin")]
     public class IndexModel : PageModel
     {
         private readonly Web_953501_Skavarodnik.Data.ApplicationDbContext _context;
